e0a44838876d23a09d58545aa3f23e63c9b835da
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2021-05-24 17:33:44 +0530

    Add files via upload

---
+++ COLOR PICKER/cp.js
@@ -0,0 +1,30 @@
+function red(){
+    document.body.style.backgroundColor="#FF0000";
+}
+function green(){
+    document.body.style.backgroundColor="#009933";
+}
+function blue(){
+    document.body.style.backgroundColor="#0066ff";
+}
+function black(){
+    document.body.style.backgroundColor="#000000";
+}
+function white(){
+    document.body.style.backgroundColor="#FFFFFF";
+}
+function yellow(){
+    document.body.style.backgroundColor="#ffff00";
+}
+function pink(){
+    document.body.style.backgroundColor="#ff66cc";
+}
+function gray(){
+    document.body.style.backgroundColor="#808080";
+}
+function lime(){
+    document.body.style.backgroundColor="#ccff33";
+}
+function maroon(){
+    document.body.style.backgroundColor="#800000";
+}
